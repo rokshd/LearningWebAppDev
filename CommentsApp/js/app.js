32335cf07aee8340345a9f589d11775ab2e83e3a
rokshd <rokshd@gmail.com>
2017-05-26 00:51:09 +0300

    refactoring

--- CommentsApp/js/app.js
+++ CommentsApp/js/app.js
@@ -1,17 +1,21 @@
 var main = function() {
-  $(".comment-input button").on("click", function(event) {
+  var AddCommentFromInputBox = function() {
     if ($(".comment-input input").val() != "") {
       var $new_comment = $("<p>").text($(".comment-input input").val());
+      $new_comment.hide();
       $(".comments").append($new_comment);
+      $new_comment.fadeIn();
       $(".comment-input input").val("");
     }
+  }
+
+  $(".comment-input button").on("click", function(event) {
+    AddCommentFromInputBox();
   });
 
   $(".comment-input input").on("keypress", function(event) {
-    if ($(".comment-input input").val() != "" && event.keyCode == 13) {
-      var $new_comment = $("<p>").text($(".comment-input input").val());
-      $(".comments").append($new_comment);
-      $(".comment-input input").val("");
+    if (event.keyCode == 13) {
+      AddCommentFromInputBox();
     }
   });
 };
