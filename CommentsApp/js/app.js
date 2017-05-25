7950bd1fdac5ee99a0fc00e689f158b509b2a3ad
rokshd <rokshd@gmail.com>
2017-05-26 00:32:41 +0300

    add comment by press key

--- CommentsApp/js/app.js
+++ CommentsApp/js/app.js
@@ -6,6 +6,14 @@ var main = function() {
       $(".comment-input input").val("");
     }
   });
+
+  $(".comment-input input").on("keypress", function(event) {
+    if ($(".comment-input input").val() != "" && event.keyCode == 13) {
+      var $new_comment = $("<p>").text($(".comment-input input").val());
+      $(".comments").append($new_comment);
+      $(".comment-input input").val("");
+    }
+  });
 };
 
 $(document).ready(main);
